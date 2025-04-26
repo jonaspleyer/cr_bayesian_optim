6f6986f1e474de1e3e6a7c1b9c341a18ae4d510f
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-26 22:43:49 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ mod sim_branching;
 
 use sim_branching::*;
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn cr_bayesian_optim(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_sim_branching, m)?)?;
