6cf6b58e45400cd65414771d9343d3630fb6204d
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-26 19:19:44 +0200

    export cellular_raza specific types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use cellular_raza::prelude as cr;
 use pyo3::prelude::*;
 
 mod sim_branching;
@@ -8,5 +9,8 @@ use sim_branching::*;
 #[pymodule]
 fn cr_bayesian_optim(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_sim_branching, m)?)?;
+    m.add_class::<cr::NewtonDamped2D>()?;
+    m.add_class::<cr::MorsePotential>()?;
+    m.add_class::<cr::CellIdentifier>()?;
     Ok(())
 }
